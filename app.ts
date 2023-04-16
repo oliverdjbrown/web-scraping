5523aee26869aceaf1610d8b826bcf315fbc36e8
Oliver Brown <oliverbrodev@gmail.com>
2023-04-16 12:32:48 -0400

    crapping lottery

--- app.ts
+++ app.ts
@@ -1,29 +1,33 @@
-const axios = require('axios');
-const cheeiro = require('cheerio');
-const fs = require('fs');
+const axios = require("axios");
+const cheeiro = require("cheerio");
+const fs = require("fs");
 
-const url = 'https://imdb.com/chart/top';
+const url =
+  "https://loterialeidsadehoy.com/quiniela-pale-del-15-de-abril-del-2023/";
 
-const movies: any = {};
+const lottery: any[] = [];
 
 const getHTML = async () => {
-    const { data: html } = await axios.get(url);
-    return html;
-}
+  const { data: html } = await axios.get(url);
+  return html;
+};
 
-getHTML().then((res)=> {
-    const $ = cheeiro.load(res);
-    $('.lister-list>tr').each((i: number, movie: any) => { 
-        const title = $(movie).find('.titleColumn>a').text();
-        const rating = $(movie).find('.imdbRating>strong').text();
-        movies[title]=rating;
-    });
-    saveFile(movies, 'movies');
+getHTML().then((res) => {
+  const $ = cheeiro.load(res);
+
+  const $title = $(".entry-title").text();
+  const $results = $(".n-resultados").text();
+
+  lottery.push({ title: $title, results: $results });
+
+  console.log(lottery);
+
+  saveFile(lottery, "lottery");
 });
 
-const saveFile = (data: any, fileName: string) => { 
-    fs.writeFile(`${fileName}.json`, JSON.stringify(data), (err: Error) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-    });
-}
+const saveFile = (data: any, fileName: string) => {
+  fs.writeFile(`${fileName}.json`, JSON.stringify(data), (err: Error) => {
+    if (err) throw err;
+    console.log("The file has been saved!");
+  });
+};
